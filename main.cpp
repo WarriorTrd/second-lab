a4c568f966fc42ee7d5db1c47a169f14b98dd96c
o.safarov <o.safarov@newuu.uz>
2025-01-23 12:42:42 +0500

    try

--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
+#include <iomanip>
+  using namespace std;
+  int main() {
+      //       string name;
+      //       std::cout<<"PLEASE enter your name: ";
+      //       std::cin>>name;
+      //       std::cout<<"okay"<<name<<"enter your age"<<endl;
+      //       int age;
+      //       std::cin>>age;
+      //       std::cout<<"okay"<<name<<"you are "<<age<<"years old"<<endl;
+      //
+      //
+      // return 0;
+      //   }
+      // for (int i = 1; i <= 5; i++) {
+      //     std::cout<<setw(5)<< i;
+      // }
+      //     return 0;
+      //
+      //     int n1;
+      //     int n2;
+      //     std::cout<<"enter a value for number1";
+      //     std::cin >> n1;
+      //     std::cout<<"enter a value for number2";
+      //     std::cin >> n2;
+      //     int temp=n1;
+      //     n1=n2;
+      //     n2=temp  ;
+      //     std::cout<<n1<<"is number1"<<setw(5)<<n2<<"is number2";
+      //
+      //     return 0;
+      // }
+      // int n1;
+      // std::cout<<"enter a number:"<<endl;
+      // std::cin >> n1;
+      // for (int i = 1; i <= n1; i++) {
+      //     std::cout<<setw(5)<<i<<setw(5)<<i*i<<endl;
+      // }
+      //
+      // return 0;
+      // }
+      string text;
+      cout << "enter your text" << endl;
+      cin>>text;
+      int width= 50;
+      int padding=(width-text.length())/2;
+      cout<<setw(padding)<<text<<setw(padding)<<endl;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+      return 0;
+  }
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-}
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
