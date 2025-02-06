b8129c14a8ca7aa8dc5b9aa24f55bd6db14c645a
o.safarov <o.safarov@newuu.uz>
2025-02-06 12:36:12 +0500

    lab 4

--- main.cpp
+++ main.cpp
@@ -2,50 +2,290 @@
 #include <iomanip>
   using namespace std;
   int main() {
-      //       string name;
-      //       std::cout<<"PLEASE enter your name: ";
-      //       std::cin>>name;
-      //       std::cout<<"okay"<<name<<"enter your age"<<endl;
-      //       int age;
-      //       std::cin>>age;
-      //       std::cout<<"okay"<<name<<"you are "<<age<<"years old"<<endl;
+      // int number;
       //
-      //
-      // return 0;
-      //   }
-      // for (int i = 1; i <= 5; i++) {
-      //     std::cout<<setw(5)<< i;
+      // cout<<"please enter a number:"<<endl;
+      // cin>>number;
+      // if (number>0) {
+      //     cout<<"number is positive"<<endl;
       // }
-      //     return 0;
-      //
-      //     int n1;
-      //     int n2;
-      //     std::cout<<"enter a value for number1";
-      //     std::cin >> n1;
-      //     std::cout<<"enter a value for number2";
-      //     std::cin >> n2;
-      //     int temp=n1;
-      //     n1=n2;
-      //     n2=temp  ;
-      //     std::cout<<n1<<"is number1"<<setw(5)<<n2<<"is number2";
-      //
-      //     return 0;
+      // else if (number<0) {
+      //     cout<<"number is negative"<<endl;
       // }
+      // else {
+      //     cout<<"zero is non negative number"<<endl;
+      // }
+
+
+
+
+
+
+
+
+
+
+
+
+      // int n,remainder;
+      // cout << "Enter a number: ";
+      // cin >> n;
+      // remainder= n %2;
+      // if (remainder==0) {
+      //     cout<<n<<" is an even number."<<endl;
+      // }
+      // else {
+      //     cout<<n<<" is an odd number."<<endl;
+      // }
+
+
+
+//       int n1,n2;
+//       cout<<"enter the first number:";
+//       cin>>n1;
+//       cout<<"enter the second number:";
+//       cin>>n2;
+//       if (n1>n2) {
+//           cout<<n1<<" is greater than "<<n2;
+//       }
+      // else if (n1=n2) {
+      //     cout<<"they are equal"<<endl;
+      // }
+// else {
+//     cout<<n2<<" is greater than "<<n1;
+// }
+
+
+
+// int  n1,n2,n3;
+//       cout << "Enter the first number: ";
+//       cin >> n1;
+//       cout << "Enter the second number: ";
+//       cin >> n2;
+//       cout << "Enter the third number: ";
+//       cin >> n3;
+//       if (n1>n2 && n1>n3) {
+//           cout<<n1<<"is the max number";
+//       }
+//       else if (n2>n1 && n2>n3) {
+//           cout<<n2<<"is the max number";
+//       }
+//       else {
+//           cout<<n3<<"is the max number";
+//       }
+
+
+
+      // int n1,n2,n3;
+      // cout<<"enter the year";
+      // cin>>n1;
+      // n2=n1%4;
+      // n3=n1%100;
+      // if (n2==0 && n3!=0) {
+      //     cout<<"it is  a leap year";
+      // }
+      // else {
+      //     cout<<"it is a not leap year";
+      // }
+      //
+      //
+
+
+
+
       // int n1;
-      // std::cout<<"enter a number:"<<endl;
-      // std::cin >> n1;
-      // for (int i = 1; i <= n1; i++) {
-      //     std::cout<<setw(5)<<i<<setw(5)<<i*i<<endl;
+      // cout << "Enter the speed: ";
+      // cin >> n1;
+      // if (n1 < 20) {
+      //     cout<<"too slow";
       // }
+      // else if (n1>80) {
+      //     cout<<"too fast";
+      // }
+      // else {
+      //     cout<<"right speed";
+      // }
+
+
+      // int n;
+      // cout<<"enter the mark u got";
+      // cin>>n;
+      // if (n>=50) {
+      //     cout<<"you passed the exam";
+      // }
+      // else {
+      //     cout<<"you failed the exam";
+      // }
+
+
+
+
+
+      // int number;
       //
-      // return 0;
+      // cout<<"please enter a number:"<<endl;
+      // cin>>number;
+      // if (number>0) {
+      //     cout<<"number is positive"<<endl;
       // }
-      string text;
-      cout << "enter your text" << endl;
-      cin>>text;
-      int width= 50;
-      int padding=(width-text.length())/2;
-      cout<<setw(padding)<<text<<setw(padding)<<endl;
+      // else if (number<0) {
+      //     cout<<"number is negative"<<endl;
+      // }
+      // else {
+      //     cout<<"zero "<<endl;
+      // }
+
+
+
+      // char n;
+      // cout<<"enter the letter /g/y/r ";
+      // cin>>n;
+      // if (n=='g') {
+      //     cout<<"Go";
+      // }
+      // else if (n=='y') {
+      //     cout<<"get ready";
+      // }
+      // else if (n=='r') {
+      //     cout<<"stop";
+      // }
+      // else {
+      //     cout<<"wrong input";
+      // }
+
+
+
+      // int n;
+      // cout<<"enter the mark u got";
+      // cin>>n;
+      // if (n>=90 && n<=100) {
+      //     cout<<"u got an A";
+      // }
+      // else if (n>=80 && n<=89) {
+      //     cout<<"u got a B";
+      // }
+      // else if (n>=70 && n<=79) {
+      //     cout<<"u got a C";
+      // }
+      // else if (n>=60 && n<=69) {
+      //     cout<<"u got a D";
+      // }
+      // else {
+      //     cout<<"u got a F";
+      // }
+
+
+
+      //
+      // int n1,n2,n3;
+      // cout<<"enter the first number:";
+      // cin>>n1;
+      // cout<<"enter the second number:";
+      // cin>>n2;
+      // n3=n1%n2;
+      // if (n3==0) {
+      //     cout<<n1<<"is divisable by"<<n2;
+      //
+      // }
+      // else {
+      //     cout<<n1<<"is not divisable by"<<n2;
+      // }
+
+
+      // int n1,n2,n3,sum1,sum2,sum3;
+      // cout<<"enter the lenght of first side of the triangle";
+      // cin>>n1;
+      // cout<<"enter the lenght of second side of the triangle";
+      // cin>>n2;
+      // cout<<"enter the lenght of third side of the triangle";
+      // cin>>n3;
+      // sum1=n1+n2;
+      // sum2=n1+n3;
+      // sum3=n2+n3;
+      // if (sum1>=n3 && sum2>=n2 && sum3>=n1) {
+      //     cout<<"your triangle is valid";
+      // }
+      // else {
+      //     cout<<"your triangle is not valid";
+      // }
+
+
+      // char n;
+      // cout<<"enter your letter";
+      // cin>>n;
+      // if (n>='A' && n<='Z') {
+      //     cout<<"your letter is uppercase "<<endl;
+      // }
+      // else if (n>='a' && n<='z') {
+      //     cout<<"your letter is lowercase "<<endl;
+      // }
+      // else {
+      //     cout<<"your input is not a letter";
+      // }
+
+
+
+
+
+
+// int price1,weight1,price2,weight2,ratio1,ratio2;
+//         cout<<"enter the price for the first rice";
+//         cin>>price1;
+//         cout<<"enter the weight for the first rice";
+//         cin>>weight1;
+//         cout<<"enter the price for the second rice";
+//         cin>>price2;
+//         cout<<"enter the weight for the second rice";
+//         cin>>weight2;
+//         ratio1=price1/weight1;
+//         ratio2=price2/weight2;
+//         if(ratio1>ratio2) {
+//               cout<<"first on is the better option";
+//
+//         }
+//         else {
+//               cout<<"second on is the better option";
+//
+//         }
+
+
+
+
+        int n,n2,n3;
+        cout<<"enter a three digit number: ";
+        cin>>n;
+        n2=n/100;
+        n3=n%10;
+        if (n2==n3) {
+              cout<<"the number is palindrome";
+        }
+        else {
+              cout<<"the number is not palindrome";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       return 0;
   }
